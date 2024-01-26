643d4d5fd6da5f727876bc6216143e2edb241d37
vinaysingh-chaudhary <vinay.heyworld@gmail.com>
2024-01-27 03:54:08 +0530

    aesthetics check

---
+++ app/loading.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const loading = () => {
+  return (
+    <div className='h-screen w-screen flex justify-center items-center bg-black'>
+      <h1 className='text-4xl text-white '>Loading Users Wait Brother</h1>
+    </div>
+  )
+}
+
+export default loading
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
-export default function Home() {
+import Image from 'next/image';
+import uchslogo from '../public/uchslogo.png';
+
+const Home = () => {
   return (
-    <div>
-      <h1>is this working ?</h1>
+    <div className=" w-screen h-screen bg-black flex justify-center items-center flex-col text-white text-3xl md:text-5xl">
+      <Image src={uchslogo} alt="uchslogo" className='w-48' />
+      <h1>use<span className='text-[#62F983]'>Custom</span>HookSpace</h1>
+      <h1 className='text-lg'>coming soon...</h1>
     </div>
   );
-}
+};
+
+export default Home;
