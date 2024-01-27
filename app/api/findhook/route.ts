8737a40c4664546433a0735f1c517debdb0dbdfd
vinaysingh-chaudhary <vinay.heyworld@gmail.com>
2024-01-28 05:00:36 +0530

    API Route error fix#1

--- app/api/findhook/route.ts
+++ app/api/findhook/route.ts
@@ -8,15 +8,9 @@ export async function GET(request : NextRequest) {
     const {hookname} = await request.json();
     const hook = await customhooks.findOne({ hookname: hookname });
 
-    if (hook) {
-      return NextResponse.json({
+    return NextResponse.json({
         data: hook,
       });
-    } else {
-      return NextResponse.json({
-        message: "no data found",
-      });
-    }
   } catch (error) {
     console.log(error);
   }
