c56748a45ae51bd265bde3a8f87bcfde1cd4f524
vinaysingh-chaudhary <vinay.heyworld@gmail.com>
2024-02-26 01:11:51 +0530

    📦🔨(Database utilites) folder structure refactored

--- app/component/SideBar/sidebar.tsx
+++ app/component/SideBar/sidebar.tsx
@@ -2,7 +2,7 @@
 import React from "react";
 import Link from "next/link";
 import { usePathname } from 'next/navigation'
-import { hooks } from "@/Static/hooks";
+import { hooks } from "../../../Static/hooks";
 
 const Sidebar =  () => {
     const router = usePathname();
