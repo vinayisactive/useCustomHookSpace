e2886863a7cf26d6c60529ce5c6fe3e767d58d33
vinaysingh-chaudhary <vinay.heyworld@gmail.com>
2024-02-28 03:44:28 +0530

    🔨API route fixes

--- app/api/blog/route.ts
+++ app/api/blog/route.ts
@@ -2,11 +2,12 @@ import { BlogModel } from "@/Schema/BlogSchema";
 import connectMongoDB from "@/DBconnection/connectMongoDB";
 import { NextResponse, NextRequest } from "next/server";
 
-export const GET = async () => {
+export const POST = async (request: NextRequest) => {
   try {
     await connectMongoDB();
+    const {id} = await request.json(); 
 
-    const blog = await BlogModel.findOne({"_id": "65de151ef4fb3346b523f22e"});
+    const blog = await BlogModel.findOne({"_id": id});
     return NextResponse.json(blog); 
   } catch (error) {
     return NextResponse.json({ error: error }, { status: 500 });
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: 'cdn-images-1.medium.com',
+            port: '',
+            pathname: '',
+          },
+        ],
+      },
+};
 
 export default nextConfig;
