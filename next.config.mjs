819004d18411e34e73c1d240ec7e3e8e24d0232d
vinaysingh-chaudhary <vinay.heyworld@gmail.com>
2024-03-03 23:37:56 +0530

    issue fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-        remotePatterns: [
-          {
-            protocol: 'https',
-            hostname: 'cdn-images-1.medium.com',
-            port: '',
-            pathname: '',
-          },
-        ],
-      },
-};
+const nextConfig = {};
 
 export default nextConfig;
