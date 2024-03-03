4ce97de43383569bf37c3e5351335cf8cdae6d74
vinaysingh-chaudhary <vinay.heyworld@gmail.com>
2024-03-04 00:07:37 +0530

    Sidebar with new UI changes added

--- app/(pages)/customhooks/layout.tsx
+++ app/(pages)/customhooks/layout.tsx
@@ -1,4 +1,4 @@
-
+import Sidebar from "@/app/component/Sidebar/Sidebar";
 
 export default async function CustomhookpageLayout({
     children,
@@ -8,7 +8,7 @@ export default async function CustomhookpageLayout({
     return (
         <div className="relative h-[92vh] flex justify-end lg:px-8 xl:px-16">
             <div className="h-full  2xl:w-[20%] xl:w-[20%] lg:w-[20%] hidden lg:flex fixed left-14">
- 
+                <Sidebar />
             </div>
             <div className=" w-full xl:w-[77%] 2xl:w-[78%] lg:w-[75%] scroll-smooth px-2 pt-4">
                 {children}
