9c4adc86497aa227c8f3b390e53b9d3860230ca2
vinaysingh-chaudhary <vinay.heyworld@gmail.com>
2024-03-04 20:38:26 +0530

    ✨🔮Config changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { };
+const nextConfig = {
+    images: {
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: 'res.cloudinary.com',
+            port: '',
+            pathname: '/dbs9ulw2r/image/upload/**',
+          },
+        ],
+      },
+};
 
 export default nextConfig;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,7 +17,13 @@ const config: Config = {
         'modal-dark' : '#070707',
         'hover-gray': '#2c2c2ca7',
         'animation-gray' : '#121212bd',
-        'code-dark' : '#2b2b2b'
+        'code-dark' : '#2b2b2b',
+        'btn-dark': '#202020a1',
+        'via-dark': '#000000d8',
+        'end-dark': '#0000002f',
+        'bnr-btn':'#303030',
+        'bnr-btn-bg': '#1c1c1c6f',
+        'bnr-btn-text' : '#8d8d8d'
       },
     },
   },
