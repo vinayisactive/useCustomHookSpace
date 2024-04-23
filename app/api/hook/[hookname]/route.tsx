8d7e6ec2e1e61e1187244583e5a2c493c8dc80ff
Vinay chaudhary <vinay.heyworld@gmail.com>
2024-04-23 17:57:21 +0530

    🌱🔨Modified (api): added response status

--- app/api/hook/[hookname]/route.tsx
+++ app/api/hook/[hookname]/route.tsx
@@ -2,14 +2,16 @@ import { customhooks } from "@/models/uCHS.model";
 import dbconnect from "@/database/dbconnect";
 import { NextResponse, NextRequest } from "next/server";
 
- 
 
 dbconnect();
-
 export const GET = async (request: NextRequest, { params }: {params: { hookname: string }}) : Promise<NextResponse>=> {
   try {
+
     const hook = await customhooks.findOne({ "hookname": params.hookname });
-    return NextResponse.json({ hook }); 
+    if(!hook)
+      return NextResponse.json({error: `${params.hookname} is coming soon`}, {status: 500}); 
+
+    return NextResponse.json({ hook },{status:200}); 
   } catch (error) {
     return NextResponse.json({ error }, { status: 500 });
   }
