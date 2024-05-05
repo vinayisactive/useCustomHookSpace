8d7f1d5ddf73c4c1ab64a5ac860d7d69a5bf4dc0
Vinay chaudhary <vinay.heyworld@gmail.com>
2024-05-05 19:25:52 +0530

    🌱✨UI: Listing cards size

--- app/component/ListingCards/ListingCards.tsx
+++ app/component/ListingCards/ListingCards.tsx
@@ -7,7 +7,7 @@ interface ListingCardsProps {
 
 const ListingCards: React.FC<ListingCardsProps> = ({ hook }) => {
   return (
-    <div className="relative w-[90%] md:w-[48%] h-28 flex justify-center items-center overflow-hidden text-3xl border rounded-xl  hover:cursor-pointer group">
+    <div className="relative w-[90%] md:w-[48%] h-20 flex justify-center items-center overflow-hidden text-3xl border rounded-xl  hover:cursor-pointer group">
         <div className=" h-full w-[0%] group-hover:w-full absolute top-0 left-0 transition-all duration-500 delay-50 bg-animation-gray z-0"></div>
 
         <Link href={`/customhooks/${hook}`} className="w-full h-full flex justify-center items-center z-[10] ">
