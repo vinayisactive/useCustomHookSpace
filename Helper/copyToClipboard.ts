9e9acf746994cafd97efd0169cf25a49fddd46fe
Vinay chaudhary <vinay.heyworld@gmail.com>
2024-10-27 02:34:39 +0530

    Update: included files in tsconfig

--- Helper/copyToClipboard.ts
+++
@@ -1,7 +0,0 @@
-export const copyToClipboard = async (textToCopy : string) => {
-    try {
-      await navigator.clipboard.writeText(textToCopy);
-    } catch (err) {
-      console.error("Unable to copy to clipboard", err);
-    }
-  };
